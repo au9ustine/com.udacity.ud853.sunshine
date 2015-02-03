0d0bdbd09273cbed0f03540ec9db9a0eabd133e1
au9ustine <duke.augustine@gmail.com>
2015-02-04 02:20:38 +0800

    Lesson 2: completed

---
+++ app/src/main/java/com/example/android/sunshine/app/WeatherDataParser.java
@@ -0,0 +1,29 @@
+package com.example.android.sunshine.app;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * WeatherDataParser
+ *
+ * Created by shaotch on 2/3/2015.
+ */
+public class WeatherDataParser {
+    /**
+     * Given a string of the form returned by the api call:
+     * http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&unit...
+     * retrieve the maximum temperature for the day indicated by dayIndex
+     * (Note: 0-indexed, so 0 would refer to the first day).
+     */
+    public static double getMaxTemperatureForDay(String weatherJsonStr, int dayIndex)
+            throws JSONException {
+        JSONObject weather = new JSONObject(weatherJsonStr);
+        JSONArray days = weather.getJSONArray("list");
+        JSONObject dayInfo = days.getJSONObject(dayIndex);
+        JSONObject temperatureInfo = dayInfo.getJSONObject("temp");
+        return temperatureInfo.getDouble("max");
+    }
+
+
+}
